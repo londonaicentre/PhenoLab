33587f3e3996666762cb17d8a7ae5f2e91968b6c
drjzhn <drjzhn@gmail.com>
2024-09-19 11:14:34 +0100

    added helper functions and script to load 2022 cambridge comorbidity codesets

--- scripts/helper.py
+++ scripts/helper.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pandas as pd
 from snowflake.snowpark import Session
 
 def confirm_env_vars(env_vars):
@@ -45,3 +46,41 @@ def select_database_schema(session, database, schema):
     except Exception as e:
         print(f"Error using database or schema: {e}")
         sys.exit(1)
+
+def load_csv_as_table(session, csv_path, table_name, database, schema):
+    """
+    Loads a CSV file as a pandas DataFrame and creates a table in Snowflake.
+        session: Snowflake session
+        csv_path: path to the CSV file
+        table_name: name of the table to be created
+        database: name of database to use
+        schema: name of the schema to use
+    """
+    try:
+        df = pd.read_csv(csv_path)
+        
+        temp_table_name = f"TEMP_{table_name}"
+        session.create_dataframe(df).write.save_as_table(temp_table_name, mode="overwrite", table_type="temporary")
+        
+        table_query = f"""
+        CREATE OR REPLACE TABLE {database}.{schema}.{table_name} AS
+        SELECT * FROM {temp_table_name};
+        """
+
+        session.sql(table_query).collect()
+
+        print(f"Table '{table_name}' created successfully in {database}.{schema}")
+
+        try:
+            query = f"SELECT * FROM {database}.{schema}.{table_name} LIMIT 10"
+            result = session.sql(query).collect()
+            print(f"Top 10 rows of {table_name}:")
+            for row in result:
+                print(row)
+        except Exception as e:
+            print(f"Error loading top 10 rows: {e}")
+            sys.exit(1)
+
+    except Exception as e:
+        print(f"Error creating table from CSV: {e}")
+        raise
