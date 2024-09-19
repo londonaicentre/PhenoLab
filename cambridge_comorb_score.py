9b17285601bac1f0fc07386aa9c5d691878ba277
drjzhn <drjzhn@gmail.com>
2024-09-19 13:01:45 +0100

    merged to DBID, switched source file format to parquet

--- cambridge_comorb_score.py
+++ cambridge_comorb_score.py
@@ -1,29 +1,80 @@
 import os
 import pandas as pd
 from dotenv import load_dotenv
-from scripts.helper import confirm_env_vars, create_snowflake_session, select_database_schema, load_csv_as_table
+from scripts.helper import confirm_env_vars, create_snowflake_session, select_database_schema, load_pq_as_table, list_tables
 import sys
 
-def list_tables(session, database, schema):
+def create_unioned_dbid_table(session, dbid_table_name):
     """
-    Lists all tables in the specified database and schema.
+    Creates a DBID lookup table for the 2022 Cambridge co-morbidity score.
+    """ 
+    try:
+        # SQL query
+        sql_query = """
+        WITH UNIONED AS (
+            SELECT 
+                "ConditionID" AS CONDITIONID,
+                "ConditionName"AS CONDITIONNAME,
+                CAST("ConceptID" AS VARCHAR) AS CONCEPTCODE,
+                "PrimaryTerm" AS PRIMARYTERM,
+                71 AS SCHEME
+            FROM INTELLIGENCE_DEV.AI_CENTRE_DEV.CAMBRIDGE_COMORB_2022_SNOMED
+            UNION ALL
+            SELECT
+                "ConditionID" AS CONDITIONID,
+                "ConditionName" AS CONDITIONNAME,
+                CAST("ProductID" AS VARCHAR) AS CONCEPTCODE,
+                "PrimaryTerm" AS PRIMARYTERM,
+                71 AS SCHEME
+            FROM INTELLIGENCE_DEV.AI_CENTRE_DEV.CAMBRIDGE_COMORB_2022_DMD
+        )
+        SELECT 
+            U.*,
+            C.DBID,
+            C.NAME AS MAPPEDNAME
+        FROM UNIONED U
+        LEFT JOIN PROD_DWH.ANALYST_PRIMARY_CARE.CONCEPT C ON U.CONCEPTCODE = C.CODE AND U.SCHEME = C.SCHEME
+        """
+
+        session.sql(f"CREATE OR REPLACE TABLE INTELLIGENCE_DEV.AI_CENTRE_DEV.{dbid_table_name} AS ({sql_query})").collect()
+        
+        print(f"Table '{dbid_table_name}' created or replaced successfully.")
+    except Exception as e:
+        print(f"Error creating or replacing table: {e}")
+        raise
+
+def analyse_missing_dbid(session, dbid_table_name):
+    """
+    Summarises missing DBID post mapping
     """
     try:
-        query = f"SHOW TABLES IN {database}.{schema}"
-        result = session.sql(query).collect()
+        sql_query = f"""
+        SELECT 
+            CONDITIONNAME,
+            COUNT(*) AS NUMBER_OF,
+            SUM(CASE WHEN DBID IS NULL THEN 1 ELSE 0 END) AS MISSING_DBID_COUNT,
+            (SUM(CASE WHEN DBID IS NULL THEN 1 ELSE 0 END) * 100.0 / COUNT(*)) AS MISSING_DBID_PERCENTAGE
+        FROM INTELLIGENCE_DEV.AI_CENTRE_DEV.{dbid_table_name}
+        GROUP BY CONDITIONNAME
+        ORDER BY MISSING_DBID_PERCENTAGE DESC
+        """
         
-        table_names = [row['name'] for row in result]
+        result = session.sql(sql_query).collect()
         
-        print(f"Tables in {database}.{schema}:")
-        for table in table_names:
-            print(f"- {table}")
+        df = pd.DataFrame(result, columns=['CONDITIONNAME', 'NUMBER_OF', 'MISSING_DBID_COUNT', 'MISSING_DBID_PERCENTAGE'])
+        
+        df['MISSING_DBID_PERCENTAGE'] = df['MISSING_DBID_PERCENTAGE'].round(2).astype(str) + '%'
+        
+        return df
+    
     except Exception as e:
-        print(f"Error listing tables: {e}")
-        sys.exit(1)
+        print(f"Error analyzing missing DBID: {e}")
+        raise
 
 def main():
     load_dotenv()
 
+    # Check that environmental variables exist
     env_vars = ["SNOWFLAKE_SERVER", "SNOWFLAKE_USER", "SNOWFLAKE_USERGROUP"]
     confirm_env_vars(env_vars)
 
@@ -34,14 +85,23 @@ def main():
     schema = "AI_CENTRE_DEV"
     select_database_schema(session, database, schema)
     
+    # Check schema connection
     list_tables(session, database, schema)
 
-    # Load CSVs from 2022 paper
-    load_csv_as_table(session, "data/camb_comorb_score/camb_comorb_snomed.csv", "CAMBRIDGE_COMORB_2022_SNOMED", database, schema)
-    load_csv_as_table(session, "data/camb_comorb_score/camb_comorb_dm+d.csv", "CAMBRIDGE_COMORB_2022_DMD", database, schema)
-    load_csv_as_table(session, "data/camb_comorb_score/camb_comorb_emismed.csv", "CAMBRIDGE_COMORB_2022_EMISMED", database, schema)
+    # Load codelists from 2022 paper (previously converted to parquet) 
+    load_pq_as_table(session, "data/camb_comorb_score/pq/dm+d codes .parquet", "CAMBRIDGE_COMORB_2022_DMD", database, schema)
+    load_pq_as_table(session, "data/camb_comorb_score/pq/EMIS medication codes.parquet", "CAMBRIDGE_COMORB_2022_EMISMED", database, schema)
+    load_pq_as_table(session, "data/camb_comorb_score/pq/SNOMED clinical terms.parquet", "CAMBRIDGE_COMORB_2022_SNOMED", database, schema)
+
+    # Create DBID table
+    dbid_table_name = "CAMBRIDGE_COMORB_2022_DBID"
+    create_unioned_dbid_table(session, dbid_table_name)
 
     list_tables(session, database, schema)
+
+    # Check how many DBIDs failed to join
+    missing = analyse_missing_dbid(session, dbid_table_name)
+    print(missing)
 
 if __name__ == "__main__":
     main()
---
+++ data/camb_comorb_score/excel_to_parquet.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import os
+from openpyxl import load_workbook
+
+def excel_to_parquet(excel_path, output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+
+    wb = load_workbook(filename=excel_path, read_only=True)
+
+    # loop through worksheets
+    for sheet_name in wb.sheetnames:
+        print(f"Processing sheet: {sheet_name}")
+
+        df = pd.read_excel(excel_path, sheet_name=sheet_name)
+
+        parquet_file_path = os.path.join(output_dir, f"{sheet_name}.parquet")
+
+        df.to_parquet(parquet_file_path, index=False)
+        print(f"Exported: {parquet_file_path}")
+
+    print("Excel to parquet conversion completed")
+
+excel_path = "BJGP.2022.0235_suppl_Appendix_1.xlsx"
+output_dir = "pq"
+
+def main():
+    excel_to_parquet(excel_path, output_dir)
+
+if __name__ == "__main__":
+    main()
