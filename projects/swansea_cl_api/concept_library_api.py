a02ab65c8577b2164a884126e100211603a437e8
Isobel Weinberg <isobelweinberg@Isobels-MacBook-Pro.local>
2025-01-13 09:59:08 +0000

    Convert to module

--- projects/swansea_cl_api/concept_library_api.py
+++ projects/swansea_cl_api/concept_library_api.py
@@ -1,34 +1,35 @@
-# pip install git+https://github.com/SwanseaUniversityMedical/pyconceptlibraryclient.git@v1.0.0
-
-from pyconceptlibraryclient import Client
+from pyconceptlibraryclient import Client # pip install git+https://github.com/SwanseaUniversityMedical/pyconceptlibraryclient.git@v1.0.0
 import pandas as pd
-client = Client(public=True)
 
+def get_phenotype_ids(search_term: str) -> pd.DataFrame:
+  client = Client(public=True)
 
-search_results = client.phenotypes.get(
-  search='asthma')
+  search_results = client.phenotypes.get(search=search_term)
 
-asthma_pheno = [item for item in search_results['data'] if 'asthma' in item['name'].lower()]
+  pheno = [item for item in search_results['data'] if search_term in item['name'].lower()]
 
-# Initialize an empty list to store codelists
-codelists = []
+  # Initialize an empty list to store codelists
+  codelists = []
 
-# Loop over each phenotype_id and get the codelist
-for phenotype in asthma_pheno:
-    phenotype_id = phenotype['phenotype_id']
-    
-    # Get codelist for each phenotype_id
-    codelist = client.phenotypes.get_codelist(phenotype_id)
-    
-    # Add phenotype_id to the codelist data
-    for item in codelist:
-        item['phenotype_id'] = phenotype_id
-    
-    # Append the codelist to the list
-    codelists.extend(codelist)
+  # Loop over each phenotype_id and get the codelist
+  for phenotype in pheno:
+      phenotype_id = phenotype['phenotype_id']
+      
+      # Get codelist for each phenotype_id
+      codelist = client.phenotypes.get_codelist(phenotype_id)
+      
+      # Add phenotype_id to the codelist data
+      for item in codelist:
+          item['phenotype_id'] = phenotype_id
+      
+      # Append the codelist to the list
+      codelists.extend(codelist)
 
-# Convert the combined codelist into a pandas DataFrame
-combined_codelist = pd.DataFrame(codelists)
+  # Convert the combined codelist into a pandas DataFrame
+  return pd.DataFrame(codelists)
 
-# Display the combined codelist
-print(combined_codelist)
+if __name__ == '__main__':
+  combined_codelist = get_phenotype_ids('asthma')
+
+  # Display the combined codelist
+  print(combined_codelist)
