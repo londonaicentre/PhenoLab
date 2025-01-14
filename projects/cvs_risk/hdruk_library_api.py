19ede3336558ef1dfc38a80c931f05de6c0cb705
Isobel Weinberg <isobel.weinberg@kcl.ac.uk>
2025-01-14 15:58:25 +0000

    Refactored search function to handle pagination

--- projects/cvs_risk/hdruk_library_api.py
+++ projects/cvs_risk/hdruk_library_api.py
@@ -29,38 +29,29 @@ def get_phenotype_codelist(phenotype_id: str, version_id: int, full_output: bool
  
   return pd.DataFrame(codes)
 
-def get_codelist_from_search_term(search_term: str) -> pd.DataFrame:
+def get_phenotpelist_from_search_term(search_term: str) -> pd.DataFrame:
   client = Client(public=True)
 
   search_results = client.phenotypes.get(search=search_term)
+  page = search_results.get('page')
+  total_pages = search_results.get('total_pages')
+  search_data = search_results.get('data')
+  
+  while page < total_pages:
+    page += 1
+    next_page = client.phenotypes.get(search=search_term, page=page)
+    search_data.extend(next_page.get('data'))
 
-  pheno = [item for item in search_results['data'] if search_term in item['name'].lower()]
+  df = pd.json_normalize(search_data, sep='_')
+  return df[['phenotype_id', 'name']]
 
-  # Initialize an empty list to store codelists
-  codelists = []
-
-  # Loop over each phenotype_id and get the codelist
-  for phenotype in pheno:
-      phenotype_id = phenotype['phenotype_id']
-      
-      # Get codelist for each phenotype_id
-      codelist = client.phenotypes.get_codelist(phenotype_id)
-      
-      # Add phenotype_id to the codelist data
-      for item in codelist:
-          item['phenotype_id'] = phenotype_id
-      
-      # Append the codelist to the list
-      codelists.extend(codelist)
-
-  # Convert the combined codelist into a pandas DataFrame
-  return pd.DataFrame(codelists)
+# TODO: Add in handling of empty search
 
 if __name__ == '__main__':
-  # combined_codelist = get_codelist_from_search_term('asthma')
+  combined_list = get_phenotypelist_from_search_term('asthma')
 
-  # # Display the combined codelist
-  # print(combined_codelist)
+  # Display the combined codelist
+  print(combined_list)
 
-  print(get_phenotype_codelist('PH189', version_id=378, full_output=True))
+  # print(get_phenotype_codelist('PH189', version_id=378, full_output=True))
 
