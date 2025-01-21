29fc1a314fb1e4da810c94c6011487e866b869cc
Isobel Weinberg <isobel.weinberg@kcl.ac.uk>
2025-01-21 16:21:07 +0000

    Refactoring phenotype storage manager for snowflake

--- projects/cvs_risk/phenotype_class.py
+++ projects/cvs_risk/phenotype_class.py
@@ -6,14 +6,21 @@ class Phenotype:
         if self.verify_data_structure(df):
             self.df = df
         else:
-            raise ValueError('Data structure does not match expected format')
-        print('Phenotype object initialised')
+            raise ValueError("Data structure does not match expected format")
+        print("Phenotype object initialised")
 
     def verify_data_structure(self, df: pd.DataFrame) -> bool:
         """Check the colums in the dataframe match those we want to put in the SQL table"""
-        print('Verifying data structure')
-        return set(df.columns) == {'phenotype_id', 'phenotype_version', 'phenotype_name', 
-                                    'concept_code', 'coding_system', 'clinical_code', 'code_description'}
-    
+        print("Verifying data structure")
+        return set(df.columns) == {
+            "phenotype_id",
+            "phenotype_version",
+            "phenotype_name",
+            "concept_code",
+            "coding_system",
+            "clinical_code",
+            "code_description",
+        }
+
     def show(self):
         pprint.pprint(self.df)
