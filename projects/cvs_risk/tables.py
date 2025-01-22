ee55c59ac9a46992fbef45f800663ae43cf01cb1
Isobel Weinberg <isobel.weinberg@kcl.ac.uk>
2025-01-22 11:06:57 +0000

    made table_name a variable

--- projects/cvs_risk/tables.py
+++ projects/cvs_risk/tables.py
@@ -1,6 +1,6 @@
 class TableQueries:
     queries = {'cvs_risk_phenotypes':"""CREATE TABLE IF NOT EXISTS cvs_risk_phenotypes (
-                record_id INTEGER AUTOINCREMENT PRIMARY KEY,
+                record_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 phenotype_id VARCHAR(30) NOT NULL,
                 phenotype_version VARCHAR(30),
                 phenotype_name VARCHAR(50),
