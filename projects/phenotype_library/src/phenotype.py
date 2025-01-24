4c6efa307403770871b59fb575bcae8208330852
Isobel Weinberg <isobel.weinberg@kcl.ac.uk>
2025-01-24 22:39:17 +0000

    making the phenotype class take dataframes and making the HDRUK api output match

--- projects/phenotype_library/src/phenotype.py
+++ projects/phenotype_library/src/phenotype.py
@@ -8,27 +8,40 @@ class VocabularyType(Enum):
     """
     Supported vocabulary types
     """
-    SNOMED = "SNOMED-CT"
-    ICD10 = "ICD-10"
+    SNOMED = "SNOMED" #SNOMED-CT
+    ICD10 = "ICD10"
     BNF = "BNF"
-    DMD = "DM+D"
+    DMD = "DMD" #DM+D
 
 class PhenotypeSource(Enum):
     """
     Sources of concept definitions
     """
-    HDRUK = "HDR UK Library"
-    LONDON = "OneLondon Terminology Server"
-    ICB_NEL = "North East London ICB Local Definition"
+    HDRUK = "HDRUK" #HDR UK Library
+    LONDON = "LONDON" #One London terminology server
+    ICB_NEL = "ICB_NEL" #North East London ICB Local Definition
+
+"""
+This dictionary deals with the problem that different APIs return different strings for their vocabularies and we want to standardise things 
+"""
+vocab_mappings = {
+    'SNOMED  CT codes': VocabularyType.SNOMED
+}
 
 @dataclass
 class Code:
+    """
+    Class to store individual codes and descriptions e.g. Code(code='49455004', code_description='Polyneuropathy co-occurrent and due to diabetes mellitus (disorder)')
+    """
     # concept level (See README.md)
     code: str
     code_description: str
 
 @dataclass
 class Codelist:
+    """
+    Class to store lists of codes
+    """
     # codelist level (See REAME.md)
     vocabulary: VocabularyType
     codelist_id: str
@@ -54,25 +67,78 @@ class Phenotype:
     version_datetime: Optional[datetime] = None
     uploaded_datetime: Optional[datetime] = None
 
+    # def __post_init__(self):
+    #     self.as_df = self.to_dataframe()
+    
+    @property
+    def df(self):
+        return self.to_dataframe()
+
     def to_dataframe(self):
+        """
+        Create a pandas dataframe for the phenotype object
+        """
         phenotype_records = []
-        for codelist in cls.codelists:
-            for code in codelist:
+        for codelist in self.codelists:
+            for code in codelist.codes:
                 record = {
-                    'CODE': code.code,
-                    'CODE_DESCRIPTION': code.code_description,
-                    'VOCABULARY': codelist.vocabulary.value,
-                    'CODELIST_ID': codelist.codelist_id,
-                    'CODELIST_NAME': codelist.codelist_name,
-                    'CODELIST_VERSION': codelist.codelist_version,
-                    'PHENOTYPE_ID': self.phenotype_id,
-                    'PHENOTYPE_NAME': self.phenotype_name,
-                    'PHENOTYPE_VERSION': self.phenotype_version,
-                    'PHENOTYPE_SOURCE': self.phenotype_source.value,
-                    'OMOP_CONCEPT_ID': self.omop_concept_id,
-                    'VERSION_DATETIME': self.version_datetime,
-                    'UPLOADED_DATETIME': self.uploaded_datetime,
+                    'code': code.code,
+                    'code_description': code.code_description,
+                    'vocabulary': codelist.vocabulary.value,
+                    'codelist_id': codelist.codelist_id,
+                    'codelist_name': codelist.codelist_name,
+                    'codelist_version': codelist.codelist_version,
+                    'phenotype_id': self.phenotype_id,
+                    'phenotype_name': self.phenotype_name,
+                    'phenotype_version': self.phenotype_version,
+                    'phenotype_source': self.phenotype_source.value,
+                    'omop_concept_id': self.omop_concept_id,
+                    'version_datetime': self.version_datetime,
+                    'uploaded_datetime': self.uploaded_datetime,
                     }
                 phenotype_records.append(record)
                 
         return pd.DataFrame(phenotype_records)
+    
+    @classmethod
+    def from_dataframe(cls, input_df):
+        """
+        Using this method, you can define a phenotype object from a dataframe. 
+        Note that the vocabulary type and phenotype source should just be strings - they will be mapped to equivalent Enums
+        """
+        expected_columns = {
+            'code',
+            'code_description',
+            'vocabulary',
+            'codelist_id',
+            'codelist_name',
+            'codelist_version',
+            'phenotype_id',
+            'phenotype_name',
+            'phenotype_version',
+            'phenotype_source',
+        }
+        input_columns = set(input_df.columns)
+        if not expected_columns.issubset(input_columns):
+            ValueError(f'Wrong columns in input. You gave {input_columns} and not {expected_columns}')
+        else:
+            phenotype_id = input_df['phenotype_id'].iloc[0] #should break if more than one unique value (and for all below)
+            phenotype_name = input_df['phenotype_name'].iloc[0]
+            phenotype_version = input_df['phenotype_version'].iloc[0]
+            phenotype_source = PhenotypeSource[input_df['phenotype_source'].iloc[0]]
+
+            codelists = []
+            for codelist_id, codelist_df in input_df.groupby('codelist_id'):
+                codes = [Code(code=row['code'], code_description=row['code_description']) for _, row in codelist_df.iterrows()]
+                codelist = Codelist(codes=codes, 
+                                    codelist_id=codelist_id, 
+                                    codelist_name=codelist_df['codelist_name'].iloc[0],
+                                    codelist_version=codelist_df['codelist_version'].iloc[0],
+                                    vocabulary=vocab_mappings[codelist_df['vocabulary'].iloc[0]])
+                codelists.append(codelist)
+
+        return cls(phenotype_id=phenotype_id, 
+                   phenotype_name=phenotype_name, 
+                   phenotype_version=phenotype_version, 
+                   phenotype_source=phenotype_source, 
+                   codelists=codelists)
