525f142e844507cba6843c7e4e0b59ba11ad0305
jjsaund <jasjotsaund@hotmail.co.uk>
2025-02-20 13:07:31 +0000

    tidied up code a bit

--- projects/meds_compliance/lipid_meds.py
+++ projects/meds_compliance/lipid_meds.py
@@ -1,15 +1,7 @@
 import streamlit as st
-import pandas as pd
-from scipy import stats
-import numpy as np
-import altair as alt
 from dotenv import load_dotenv
 from phmlondon.snow_utils import SnowflakeConnection
-import json
-import folium
-from streamlit_folium import folium_static
-import geopandas as gpd
-import plotly.graph_objects as go
+
 
 # This code when ran with streamlit run, will open a prototype dashboard that looks at lipid lowering medication prescriptions in those who are eligible for lipid lowring meds.
 # So far the code looks at 2 NICE guidance criteria, High Qrisk over 10 and Type 1 Diabetes (T1DM)
