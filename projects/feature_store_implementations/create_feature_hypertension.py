e2547affdaa965e89fc19567f3eeca9ef1a4ae27
Isobel Weinberg <isiweinberg@gmail.com>
2025-03-07 12:24:46 +0000

    Updating feature store code for static features

--- projects/feature_store_implementations/create_feature_hypertension.py
+++ projects/feature_store_implementations/create_feature_hypertension.py
@@ -6,7 +6,7 @@ from feature_store_manager import FeatureStoreManager
 load_dotenv()
 conn = SnowflakeConnection()
 DATABASE = "INTELLIGENCE_DEV"
-SCHEMA = "TEST_FEATURE_STORE_IW_2"
+SCHEMA = "AI_CENTRE_FEATURE_STORE"
 feature_store_manager = FeatureStoreManager(conn, DATABASE, SCHEMA)
 
 with open("htn_query.sql", "r") as fid:
@@ -15,9 +15,27 @@ with open("htn_query.sql", "r") as fid:
 # df = conn.session.sql(query).to_pandas()
 # # print(df.head(100))
 
-feature_store_manager.add_new_feature(
-    feature_name="Hypertension",
-    feature_desc="Hypertension either diagnosed by codelist, by ambulatory readings or by 3 clinic readings",
-    feature_format="Binary/categorical",
-    sql_select_query_to_generate_feature=query,
-    target_lag="7 days")
+# feature_store_manager.add_new_feature(
+#     feature_name="Hypertension",
+#     feature_desc=""""
+#         Hypertension either diagnosed by codelist, by ambulatory readings or by 3 clinic readings. Hypertension is 
+#         categorised as stage 1 if the patient has an ambulatory reading between 135/85 and 149/94 or 3 clinic readings 
+#         from 140/90 to 179/119 in 1 year, and as stage 2 if the patient has an ambulatory reading above 150/95 or 
+#         3 clinic readings above 180/120 in 1 year. Patients diagnosed by codelist do not have a stage 1/2 flag.
+#         """,
+#     feature_format="Binary/categorical",
+#     sql_select_query_to_generate_feature=query)
+
+fid = feature_store_manager.get_feature_id_from_table_name('hypertension_v1')
+print(fid)
+
+v = feature_store_manager.get_latest_feature_version(fid)
+print(v)
+
+v2 = feature_store_manager.update_feature(
+    feature_id=fid,
+    new_sql_select_query=query,
+    change_description="A test update; same query"
+)
+print(v2)
+
