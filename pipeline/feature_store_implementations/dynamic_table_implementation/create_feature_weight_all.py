daf01225c751c69da31ca58750b30f4d93c36f64
jjsaund <jasjotsaund@hotmail.co.uk>
2025-03-10 18:04:13 +0000

    added create_feature_xxx.py for weight/obesity feature tables.

---
+++ pipeline/feature_store_implementations/dynamic_table_implementation/create_feature_weight_all.py
@@ -0,0 +1,40 @@
+
+from dotenv import load_dotenv
+from feature_store_manager import FeatureStoreManager
+
+from phmlondon.snow_utils import SnowflakeConnection
+
+load_dotenv()
+conn = SnowflakeConnection()
+DATABASE = "INTELLIGENCE_DEV"
+SCHEMA = "AI_CENTRE_FEATURE_STORE"
+feature_store_manager = FeatureStoreManager(conn, DATABASE, SCHEMA)
+
+with open("weight_table_all.sql", "r") as fid:
+    query = fid.read()
+
+feature_store_manager.add_new_feature(
+    feature_name="Weight_all",
+    feature_desc=""""
+        Weight categorisation determined by either 1.BMI, 2.Coded category, 3.BMI calculations
+        from Height and Weight.
+        Categories: Underweight, Healthy weight, Overweight,
+        Obese unclassified, Obese class I, Obese class II, Obese class III
+        Cohort - all patients and all results.
+        """,
+    feature_format="Binary/categorical",
+    sql_select_query_to_generate_feature=query)
+
+# fid = feature_store_manager.get_feature_id_from_table_name('hypertension_v1')
+# print(fid)
+
+# v = feature_store_manager.get_latest_feature_version(fid)
+# print(v)
+
+# v2 = feature_store_manager.update_feature(
+#     feature_id=fid,
+#     new_sql_select_query=query,
+#     change_description="A test update; same query"
+# )
+# print(v2)
+
