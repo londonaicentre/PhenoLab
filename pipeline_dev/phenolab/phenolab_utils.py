9e482e598024197b6b0059ebfd048fb7f0513746
Isobel Weinberg <isiweinberg@gmail.com>
2025-03-21 11:46:44 +0000

    Progress towards find codes from existing definitions feature and created utils for reusable functions

---
+++ pipeline_dev/phenolab/phenolab_utils.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+from dotenv import load_dotenv
+
+from phmlondon.snow_utils import SnowflakeConnection
+
+@st.cache_resource(show_spinner='Connecting to Snowflake...')
+def connect_to_snowflake() -> SnowflakeConnection:
+    load_dotenv()
+
+    if "snowsesh" not in st.session_state:
+        st.session_state.snowsesh = SnowflakeConnection()
+        st.session_state.snowsesh.use_database("INTELLIGENCE_DEV")
+        st.session_state.snowsesh.use_schema("AI_CENTRE_DEFINITION_LIBRARY")
+
+    snowsesh = st.session_state.snowsesh
+    return snowsesh
+
+@st.cache_data(show_spinner='Reading from database...')
+def get_data_from_snowflake_to_dataframe(_session: SnowflakeConnection, query: str) -> pd.DataFrame:
+    return _session.execute_query_to_df(query)
+
+@st.cache_data
+def get_data_from_snowflake_to_list(_session: SnowflakeConnection, query: str) -> list:
+    return _session.session.sql(query).collect()
+
+@st.cache_data
+def get_definitions_from_snowflake_and_return_as_annotated_list_with_id_list(_session: SnowflakeConnection) -> tuple[list, list]:
+    comparison_query = f"""
+    SELECT DISTINCT DEFINITION_SOURCE, DEFINITION_ID, DEFINITION_NAME
+    FROM INTELLIGENCE_DEV.AI_CENTRE_DEFINITION_LIBRARY.DEFINITIONSTORE
+    ORDER BY DEFINITION_NAME
+    """
+    # comparison_defintions = snowsesh.execute_query_to_df(comparison_query)
+    comparison_definitions = get_data_from_snowflake_to_dataframe(_session, comparison_query)
+
+    return comparison_definitions['DEFINITION_ID'].to_list(), [f"[{row['DEFINITION_SOURCE']}] [{row['DEFINITION_ID']}] {row['DEFINITION_NAME']}" 
+                                for i, row in comparison_definitions.iterrows()]
