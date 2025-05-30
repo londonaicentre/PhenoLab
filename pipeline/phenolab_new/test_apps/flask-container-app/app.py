6faff1a026cc2dfe8db52f29d66db724e8daf314
Isobel Weinberg <isiweinberg@gmail.com>
2025-05-30 12:10:50 +0100

    Testing app pipelines

---
+++ pipeline/phenolab_new/test_apps/flask-container-app/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+# from phmlondon.snow_utils import SnowflakeConnection
+from snowflake.snowpark import Session
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # conn = SnowflakeConnection()
+    load_dotenv()
+            
+    connection_parameters = {
+            "account": os.getenv("SNOWFLAKE_SERVER"),
+            "user": os.getenv("SNOWFLAKE_USER"),
+            "role": os.getenv("SNOWFLAKE_USERGROUP"),
+            "authenticator": "externalbrowser",
+        }
+
+    session = Session.builder.configs(connection_parameters).create()
+
+    results = session.sql("SELECT * FROM PROD_DWH.ANALYST_PRIMARY_CARE.CONCEPT LIMIT 10").collect()
+    return results
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
