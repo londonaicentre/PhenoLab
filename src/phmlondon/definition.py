7025913d738e3cd958e7c43ebb3f39d954ec34fc
Isobel Weinberg <isiweinberg@gmail.com>
2025-06-03 12:44:04 +0100

    Accidental omission from previous commit

--- src/phmlondon/definition.py
+++ src/phmlondon/definition.py
@@ -456,7 +456,7 @@ class Definition:
 
         load_dotenv() # to get AWS access keys from .env file; need to replace with better authentication method
 
-        dynamodb = boto3.resource("dynamodb", region_name='eu-north-1')
+        dynamodb = boto3.resource("dynamodb", region_name='eu-west-2')
         table = dynamodb.Table("definitions")
 
         self.version_datetime = datetime.now()
@@ -469,7 +469,7 @@ class Definition:
 
         load_dotenv() # to get AWS access keys from .env file; need to replace with better authentication method
 
-        dynamodb = boto3.resource("dynamodb", region_name='eu-north-1')
+        dynamodb = boto3.resource("dynamodb", region_name='eu-west-2')
         table = dynamodb.Table("definitions")
 
         response = table.query(KeyConditionExpression=Key('definition_id').eq(definition_id), 
