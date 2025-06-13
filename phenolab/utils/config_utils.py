27148e66b15d2c2bc305f2183da583e654494504
Isobel Weinberg <isiweinberg@gmail.com>
2025-06-13 13:52:09 +0100

    Now map snowflake account name to a config

--- phenolab/utils/config_utils.py
+++ phenolab/utils/config_utils.py
@@ -2,17 +2,32 @@ import os
 
 from dotenv import load_dotenv
 import yaml
+from snowflake.snowpark import Session
 
-def load_config() -> dict:
+phenolab_config_mapping = {"SE56186": "nel_icb"}
+
+def load_config(session: Session) -> dict:
     load_dotenv(override=True)
 
-    phenolab_config = os.getenv("PHENOLAB_CONFIG")
-    if phenolab_config is None:
-        raise EnvironmentError("PHENOLAB_CONFIG environment variable is not set.")
+    # Find out which snowflake account we're on
+    account_name = session.sql("SELECT CURRENT_ACCOUNT();").collect()[0]["CURRENT_ACCOUNT()"]
+    if account_name in phenolab_config_mapping:
+        phenolab_config = phenolab_config_mapping[account_name]
+    else:
+        raise EnvironmentError("No matching configuration found for the current Snowflake account.")
+    
     with open(f"configs/{phenolab_config}.yml", "r") as fid:
-        return yaml.safe_load(fid)
+        config = yaml.safe_load(fid)
+
+        # Set whether we are developing locally or for the AI centre
+    if os.getenv("LOCAL_DEVELOPMENT", "FALSE").upper() == "TRUE":
+        config["local_development"] = True
+    else:
+        config["local_development"] = False
+
+    return config
 
 if __name__ == "__main__":
     # Example usage/for debugging
-    config = load_config()
+    config = load_config(session=Session.builder.config("connection_name", "nel_icb").create())
     print(config)
