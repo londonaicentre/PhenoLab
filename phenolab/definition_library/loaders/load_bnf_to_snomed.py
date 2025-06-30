bd20eb24c8e461a9ab9319d460b407c5b5f98502
Isobel Weinberg <isiweinberg@gmail.com>
2025-06-30 15:32:48 +0100

    BNF processed csv pregenerated for speed

--- phenolab/definition_library/loaders/load_bnf_to_snomed.py
+++ phenolab/definition_library/loaders/load_bnf_to_snomed.py
@@ -11,45 +11,13 @@ from phmlondon.snow_utils import SnowflakeConnection
 from definition_library.loaders.create_tables import load_definitions_to_snowflake
 
 
-def process_snomed_mappings(xlsx_files):
-    """
-    Combine and deduplicate mapping files
-    """
-    dfs = [pd.read_excel(f) for f in xlsx_files]
-    combined = pd.concat(dfs)
-    return combined.drop_duplicates(subset=["BNF Code", "SNOMED Code"])
-
-
-def build_definitions(file_path, mapping_files):
+def build_definitions(input_path: str) -> pd.DataFrame:
     """
     Build definitions from BNF chemical substances and SNOMED mappings
     """
-    with open(file_path) as csv_file:
-        bnf_df = pd.read_csv(csv_file, 
-            usecols=["BNF Chemical Substance", 
-                     "BNF Subparagraph Code", 
-                     "BNF Subparagraph", 
-                     "BNF Chemical Substance Code"])
-    
-    chemical_substances = bnf_df[
-        ["BNF Chemical Substance", "BNF Subparagraph Code", "BNF Subparagraph", "BNF Chemical Substance Code"]
-    ].drop_duplicates()
 
-    print("BNF dataframe created!")
+    joined_data = pd.read_csv(f"{input_path}.csv")
 
-    # process different years of BNF SNOMED mapping files
-    mappings = process_snomed_mappings(mapping_files)
-
-    mappings["Chemical Substance Code"] = mappings["BNF Code"].str[:9]
-
-    # join mappings with BNF chemical substances
-    joined_data = pd.merge(
-        mappings,
-        chemical_substances,
-        left_on="Chemical Substance Code",
-        right_on="BNF Chemical Substance Code",
-    )
-    joined_data = joined_data.dropna(subset=["SNOMED Code"])
     definitions = []
     current_datetime = datetime.now()
 
@@ -97,10 +65,8 @@ def build_definitions(file_path, mapping_files):
 def retrieve_bnf_definitions_and_add_to_snowflake(
         database: str = "INTELLIGENCE_DEV",
         schema: str = "AI_CENTRE_DEFINITION_LIBRARY"):
-                                                  
-    mapping_files = Path("definition_library/loaders/data/bnf_to_snomed/").glob("*.xlsx")
-    definition_df = build_definitions(
-        "definition_library/loaders/data/bsa_bnf/20241101_1730476037387_BNF_Code_Information.csv", mapping_files)
+
+    definition_df = build_definitions(input_path="definition_library/loaders/data/bnf_to_snomed/processed_bnf_data")
     print("Definitions built")
     definition_df.columns = definition_df.columns.str.upper()
     print("success in naming!")
