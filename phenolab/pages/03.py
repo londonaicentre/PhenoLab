f5c9f72cd164a1ed9e09edd7550c601de79c375d
Isobel Weinberg <isiweinberg@gmail.com>
2025-07-01 09:33:35 +0100

    Boto3 removed as dependency

---
+++ phenolab/pages/03.py
@@ -0,0 +1,17 @@
+# - get a list of measurements - both ICB table and AIC table (not local jsosn)
+# - select a measurement
+# - add units 
+# - map units
+# - convert units
+# - out of range
+# - regenerate table from config -> checks for if any new measurements and then does so
+# - some plots
+
+# remote table:
+# ---------
+# definition source
+# definition
+# original person, measurement, unit etc
+# mapped units
+# converted val and converted units
+# flag for out of range
