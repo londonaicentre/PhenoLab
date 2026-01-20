d1e246e6f14c152faebe9b728e73472ccb7e44aa
Dylan  Clarke <dylan.clarke1@ncld.nhs.uk>
2026-01-20 22:55:12 +0000

    fix: ensure config_utils.py is OS agnostic

--- phenolab/utils/config_utils.py
+++ phenolab/utils/config_utils.py
@@ -29,7 +29,7 @@ def load_config(session: Session = None, deploy_env: str = None) -> dict:
     """
 
     load_dotenv(override=True)
-    if os.environ['HOME'] == "/home/udf":
+    if os.path.expanduser("~") == "/home/udf":
         # This is a hideous hack, but this env variable exists on streamlit in snowflake
         local_development = False
     else:
